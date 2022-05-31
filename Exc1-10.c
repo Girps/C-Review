a024b10be1dfd7aec7160d2828bd14571e78778d
Jesse Jimenez <105522853+Girps@users.noreply.github.com>
2022-05-30 22:26:47 -0700

    Exc1-10.c solution to exercise 10 chapter 1 of C programming language

---
+++ Exc1-10.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Ex1-10
+    Write a program to copy its input to its output, replacing each
+        string of \t,\b and each backslash by \\
+*/
+
+
+int main()
+{
+    int c = 0;
+
+    // Check if end of file
+    while((c = getchar()) != EOF  )
+        {
+            if(c == '\t')
+                {
+                    putchar('\\');
+                    putchar('t');
+                }
+            else if(c == 8) //
+                {
+                    putchar('\\b');
+
+                }
+            else if(c == '\\')
+                {
+                    putchar('\\');
+                }
+                putchar(c);
+        }
+
+
+    return 0;
+}
+
